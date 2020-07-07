d987e9e97e4b5f909384feaac47dd089ed923197
greencar77 <x>
2020-07-07 14:25:24 +0300

    write String to file

--- src/main/java/com/rabarbers/htmlgen/CustomFileUtils.java
+++ src/main/java/com/rabarbers/htmlgen/CustomFileUtils.java
@@ -18,6 +18,15 @@ public class CustomFileUtils {
             throw new RuntimeException(e);
         }
     }
+    public static void writeFileWrapper(String path, String string) {
+        try {
+            File file = new File(path);
+            System.out.println("Output file: " + file.getAbsolutePath());
+            FileUtils.writeByteArrayToFile(file, string.getBytes(Charset.forName("UTF-8")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void writeFileWrapper(String path, Element element) {
         try {
